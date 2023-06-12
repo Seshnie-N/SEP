b0787d51ecda43e082fcb78fb6f8198a651ed4a8
seshnie <seshnie.naidoo26@gmail.com>
2023-06-12 23:33:49 +0200

    Created JobApplication controller and index page.

---
+++ Controllers/JobApplicationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SEP.Controllers
+{
+    public class JobApplicationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
